62dcd9c8109ad775cf4259f2d6077968fd12d091
Ilian Kostov <ilian.g.kostov@gmail.com>
2022-04-26 04:01:00 +0300

    Enter

---
+++ C/testFolder/ex.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+ int givenNumber;
+ printf("Tell me your num:\n");
+ scanf("%d",&givenNumber);
+ printf("Yes, my num is also: %i\n", givenNumber);
+ return 0;
+}
---
+++ C/testFolder/tempCodeRunnerFile.c
@@ -0,0 +1,31 @@
+
+
+	// --- Integer ---
+
+	// Input the integer
+	printf("Enter the integer: ");
+	scanf("%d", &num);
+
+	// Output the integer
+	printf("\nEntered integer is: %d", num);
+
+	// --- Float ---
+
+	// Input the float
+	printf("\n\nEnter the float: ");
+	scanf("%f", &f);
+
+	// Output the float
+	printf("\nEntered float is: %f", f);
+
+	// --- Character ---
+
+	// Input the Character
+	printf("\n\nEnter the Character: ");
+	scanf("%c", &ch);
+
+	// Output the Character
+	printf("\nEntered integer is: %c", ch);
+
+	return 0;
+}
