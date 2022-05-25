3a61c3e4e35cac5080f7745198f2c7d7b233cede
Ilian Kostov <ilian.g.kostov@gmail.com>
2022-05-25 17:18:31 +0300

    enter

--- C/17.05.22-Lab/Arrays_zapiski/5.1.ukazatel_masiv.c
+++ C/17.05.22-Lab/Arrays_zapiski/5.1.ukazatel_masiv.c
@@ -5,9 +5,8 @@ int main()
     int * ptr = arr; // указателя присвоява името на масива.
     // което означава, че към елементите от масива можем да се обръщаме чрез този указател.
 
-    arr[0] = 10; // Stores 10 at 0th element of array
-    ptr[1] = 20; // Stores 20 at 1st element of array
-
+    arr[0] = 10; // Stores 10 at 0th element of array [10, 0, 0, 0, 0]
+    ptr[1] = 20; // Stores 20 at 1st element of array [10, 20, 0, 0, 0]
     ptr = arr; // ptr and arr both points to 0th element of array
 
     *ptr = 100; // Stores 100 at 0th element of array (Since ptr points at arr[0])
