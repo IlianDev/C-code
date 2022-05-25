ab4620a819abc44eaba5ca2100f5b7ed3807c514
Ilian Kostov <ilian.g.kostov@gmail.com>
2022-05-25 16:00:37 +0300

    enter

--- C/17.05.22-Lab/Ukazateli_pointeri/3.2.void_pointer.c
+++ C/17.05.22-Lab/Ukazateli_pointeri/3.2.void_pointer.c
@@ -6,13 +6,17 @@ int main()
 
    void *ptr; // указател към неопределен тип
 
-   ptr = &a;
+   ptr = &a; // adress
    int *temp = (int *)ptr; // указател темп ще сочи там където е ptr, но след като е кастнат 
    printf("After typecasting, a = %d\n", *temp); // After typecasting, a = 2
+   printf("1.After typecasting, a = %d\n", *(int *)ptr); // After typecasting, a = 2
 
    ptr = &c;
    char *t = (char *)ptr;
-    printf("After typecasting, a = %c\n", *t); // After typecasting, a = a
+   printf("After typecasting, a = %c\n", *t); // After typecasting, a = a
+   printf("2.After typecasting, a = %c\n", *(char *)ptr); // After typecasting, a = a
+   printf("3.Adress.After typecasting, a = %u\n", (char *)ptr); // a = 4064278787
+
 
 
    return 0;
