cbcb1fb9155eaad3436ad2ac8b3e58afe5546725
Ilian Kostov <ilian.g.kostov@gmail.com>
2022-05-25 16:39:36 +0300

    enter

--- C/17.05.22-Lab/Examples_arrays/4. reverse_array.c
+++ C/17.05.22-Lab/Examples_arrays/4. reverse_array.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #define n 6
 int main(){
-    int arr[n] = {1,2,3,4,5,6,7};
+    int arr [ n ] = {1 ,2 ,3 ,4 ,5 ,6};
     int temp;
-    for(int i = 0; i<n/2; i++){
-        temp = arr[i]; // 1
-        arr[i] = arr[n-i-1]; // 6
-        arr[n-i-1] = temp; // 6 --> 1
+
+    for(int i = 0; i<n/2; i++)
+    {
+        temp = arr [ i ]; // 1
+        arr [ i ] = arr[ n-i-1 ]; // 6
+        arr [ n-i-1 ] = temp; // 6 --> 1
     }
     for(int i = 0; i < n; i++){
         printf("%d,", arr[i]);
