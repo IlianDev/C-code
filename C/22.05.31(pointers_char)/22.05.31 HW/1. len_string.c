c994eaf0ec6b47b1116cabae95496aa37d462992
Ilian Kostov <ilian.g.kostov@gmail.com>
2022-06-02 14:05:12 +0300

    enter

---
+++ C/22.05.31(pointers_char)/22.05.31 HW/1. len_string.c
@@ -0,0 +1,28 @@
+/*
+1) Напишете C програма за намиране на дължината на низ с помощта на цикъл, без да
+използвате вградена библиотечна функция strlen() .
+*/
+
+#include <stdio.h>
+#define MAX_SIZE 100 
+
+int main()
+{
+    char text[MAX_SIZE]; 
+    int i;
+    int count= 0;
+
+    /* Input a string from user */
+    printf("Enter any string: ");
+    gets(text);
+
+    /* Iterate till the last character of string */
+    for(i=0; text[i]!='\0'; i++)
+    {
+        count++;
+    }
+
+    printf("Length of '%s' = %d", text, count);
+
+    return 0;
+}
