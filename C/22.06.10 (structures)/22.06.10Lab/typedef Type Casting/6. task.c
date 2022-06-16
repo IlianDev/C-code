252598bf42f597871a8083c07a64846c2fa8675f
Ilian Kostov <ilian.g.kostov@gmail.com>
2022-06-16 13:23:46 +0300

    task

--- C/22.06.10 (structures)/22.06.10Lab/typedef Type Casting/6. task.c
+++ C/22.06.10 (structures)/22.06.10Lab/typedef Type Casting/6. task.c
@@ -1,8 +1,27 @@
+/*
+Задача 6. Напишете структура с потребителски тип key_t, която съдържа
+символен низ и число. Създайте променлива от новия тип, като
+инициализирате символния низ с динамично заделена памет за него.
+Принтирайте съдържанието на структурата.
+*/
+
 #include <stdlib.h>
+#include<stdio.h>
 
-
+typedef struct{
+    char *s;
+    int n;
+}key_t;
 
 int main()
 {
-    
+    key_t a;
+
+    a.n = 6;
+    a.s = malloc(a.n);
+    a.s = "This is something";
+
+    printf("%d \t %d", sizeof(a.n), sizeof(key_t)); // 4 16;
+
+    return 0;
 }
